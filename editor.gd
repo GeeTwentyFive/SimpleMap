df2be94cf41e0967f8d74b105c2a78eff9a43768
Gee <alickoamarko@gmail.com>
2025-10-26 16:25:00 +0100

    Added CTRL+D MapObject duplication support

--- editor.gd
+++ editor.gd
@@ -262,10 +262,19 @@ func _ready() -> void:
 	RegisterMapObjectsFromDir(OS.get_cmdline_args()[0])
 
 func _input(event: InputEvent) -> void:
-	if (event is InputEventKey and event.pressed):
+	if (event is InputEventKey and event.pressed and not event.is_echo()):
 		match event.keycode:
 			KEY_ESCAPE: get_viewport().gui_release_focus()
 			KEY_DELETE: DeleteSelectedMapObject()
+			KEY_D:
+				if Input.is_key_pressed(KEY_CTRL) and selected_map_object:
+					SelectMapObject(InstantiateMapObject(
+						selected_map_object.get_meta("path"),
+						selected_map_object.position,
+						selected_map_object.rotation_degrees,
+						selected_map_object.scale,
+						selected_map_object.get_meta("extra_data")
+					))
 
 func _physics_process(_delta: float) -> void:
 	if get_viewport().gui_get_focus_owner() != null: return
